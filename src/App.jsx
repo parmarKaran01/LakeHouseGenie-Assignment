eccc5d67bfaa8839ece2085ab1e0dd0d6bcc4c13
Karan Parmar <parmarkaran1976@gmail.com>
2023-01-17 14:54:38 +0530

    fixed the import error

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
-import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
-import CommonElement from "./components/CommonElement";
-
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
